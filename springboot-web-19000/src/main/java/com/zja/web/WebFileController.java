cb9e743cfbd25ef193ab83d6358b8b879d35495c
郑家骜[ào] <zhengja@dist.com.cn>
2025-06-30 15:12:15 +0800

    fix 将javax.servlet替换为jakarta.servlet，以解决Spring Boot 3中因Servlet API包名从javax迁移到jakarta导致的类找不到问题。

--- springboot-web-19000/src/main/java/com/zja/web/WebFileController.java
+++ springboot-web-19000/src/main/java/com/zja/web/WebFileController.java
@@ -17,9 +17,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URLEncoder;
 import java.nio.ByteBuffer;
